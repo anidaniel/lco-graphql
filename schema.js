2f7c73c7894183418774a7199cb110c0acea35a1
Anidaniel <anidaniel001@gmail.com>
2021-07-13 00:06:36 +0530

    changes

---
+++ schema.js
@@ -0,0 +1,33 @@
+import { buildSchema } from "qraphql";
+
+const schema = buildSchema(`
+    type Course {
+        id: ID
+        courseName: String
+        category: String
+        price: Int
+        language: String
+        email: String
+        stack: Stack
+        teachingAssists: [TeachAssist]
+    }
+
+    type TeachAssist {
+        firstName: String
+        lastName: String
+        experience: Int
+    }
+
+    enum Stack {
+        WEB
+        MOBILE
+        OTHER
+    }
+
+    type Query {
+        getCourse(id: ID): Course
+    }
+
+`);
+
+export default schema;
